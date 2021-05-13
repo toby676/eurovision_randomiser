bee81eed7fb8267c4cc3ae41aab92cea3d36c536
Toby Muir-Little <toby.muirlittle@simplybusiness.co.uk>
2021-05-13 20:35:12 +0100

    add country model

---
+++ app/models/country.rb
@@ -0,0 +1,9 @@
+class Country
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :country_name, type: String
+  field :flag_asset_url, type: String
+  field :congratulations, type: String
+  field :rehearsal_link, type: String
+end
